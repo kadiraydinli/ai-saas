87e04dd0e9afd856ddd0020bbe82be2ed72dd2fe
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-07-19 16:53:47 +0300

    clerk authentication setup

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 const LandingPage = () => {
-	return <div>LandingPage</div>;
+	return (
+		<div>
+			LandingPage
+			<div>
+				<Link href="/sign-in">
+					<Button>Login</Button>
+				</Link>
+				<Link href="/sign-up">
+					<Button>Register</Button>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default LandingPage;
