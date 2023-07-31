96f19ac656c15d73c951068feca28b53b037b70f
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-08-01 01:03:45 +0300

    add user api limit feature

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
 import { auth } from "@clerk/nextjs";
 
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+
 const configuration = new Configuration({
     apiKey: process.env.OPEN_AI_KEY,
 });
@@ -36,11 +38,19 @@ export async function POST(req: Request) {
             return new NextResponse("Resolution is required", { status: 400 });
         }
 
+        const freeTrial = await checkApiLimit();
+
+        if (!freeTrial) {
+            return new NextResponse("Free trial has expired!", { status: 403 });
+        }
+
         const response = await openai.createImage({
             prompt: prompt,
             n: parseInt(amount, 10),
             size: resolution,
         });
+
+        await increaseApiLimit();
 
         return NextResponse.json(response.data.data);
     } catch (error) {
